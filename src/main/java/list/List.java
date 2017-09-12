480d7da539866f5fd29b1d9b05c543ecbe597274
NormaN <normanargiolas@gmail.com>
2017-09-12 16:47:04 +0200

    first

---
+++ src/main/java/list/List.java
@@ -0,0 +1,13 @@
+package list;
+
+public class List {
+    public static void main(String args[]) throws Exception {
+        System.out.println("----------start");
+        List app = new List();
+        app.run();
+    }
+
+    void run() throws Exception {
+        System.out.println("----------end");
+    }
+}
